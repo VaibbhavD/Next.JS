e728248dd2be67a6add4d8fb44863cbec69a4d24
VaibhavDhamanage <vaibhavdhamanage>
2024-03-06 12:40:53 +0530

    define dummy array of Meetups and send to meetup list component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,33 @@
-function MyApp({ Component, pageProps }) {
-  return <h1>Vaibhav</h1>;
-}
+import MeetupList from "@/components/meetups/MeetupList";
 
-export default MyApp;
+const Dummy_List = [
+  {
+    id: "a1",
+    title: "First Meet",
+    image: "https://wallpaperaccess.com/full/1137443.jpg",
+    address: "New York ",
+  },
+  {
+    id: "a2",
+    title: "Second Meet",
+    image:
+      "https://www.tripsavvy.com/thmb/aQu3cON2Y7jiaR74d_5s8ZOyvtw=/3600x2398/filters:fill(auto,1)/GettyImages-859451446-a27a2e17ac3640bdb36179083e7b6818.jpg",
+    address: "Paries",
+  },
+  {
+    id: "a3",
+    title: "Third Meet",
+    image:
+      "https://www.hdwallpaper.nu/wp-content/uploads/2015/07/869c978552ff253563b883e6f808f066.jpg",
+    address: "America",
+  },
+];
+
+function HomePage() {
+  return (
+    <>
+      <MeetupList meetups={Dummy_List} />
+    </>
+  );
+}
+export default HomePage;
