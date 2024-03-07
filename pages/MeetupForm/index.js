e353bb3d69599ad3baafe499544ddcea1156b4db
VaibhavDhamanage <vaibhavdhamanage>
2024-03-07 13:06:57 +0530

    Recive data from MeetUp Form

--- pages/MeetupForm/index.js
+++ pages/MeetupForm/index.js
@@ -1,9 +1,24 @@
 import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 
 function MeetupForm() {
+  const router = useRouter();
+  async function onAddMeetup(Meetupdata) {
+    const response = await fetch("api/new-meetup-apis/", {
+      method: "POST",
+      body: JSON.stringify(Meetupdata),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    router.push("/");
+  }
+
   return (
     <>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={onAddMeetup} />
     </>
   );
 }
